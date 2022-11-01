7a54f7621e0213c217b98ddad4a6adeb9c323435
Bentike <ajadiyusuf73@gmail.com>
2022-11-01 19:01:34 +0100

    added message prop to Buttons Component

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Button from './Button';
 import './Styles/Buttons.css';
 
-function Buttons({setHard, setEasy, reset}) {
+function Buttons({setHard, setEasy, reset, message}) {
   return (
     <div className='buttons'>
       <div className='reset'>
         <Button text='New Colors' color='green' handleClick={reset}/>
-        <p className='message'></p>
+        <p className='message'>{message}</p>
       </div>
       <div className='level'>
         <Button text='EASY' color='green' handleClick={setEasy}/>
