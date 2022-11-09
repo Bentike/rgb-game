9cfab333b662dfe396410b0057de28d68b7455b1
Bentike <ajadiyusuf73@gmail.com>
2022-11-09 17:26:17 +0100

    debugging pickedColor variable in redux

--- src/reducers.js
+++ src/reducers.js
@@ -42,6 +42,7 @@ export const boxColorReducer = (state = initialState, action) => {
         case GET_BOX_COLOR: 
            // line 15 gives access to the background of the clicked box.
            let pickedColor = action.payload.target.style.backgroundColor;
+           console.log(pickedColor);
            return Object.assign({}, state, {pickedColor}); 
 
         case SET_GAME_LEVEL_EASY:
