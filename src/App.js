275406abcd60dbde856e7b674ce535a245a6d6ef
Bentike <ajadiyusuf73@gmail.com>
2022-11-14 12:44:25 -0800

    removed argument from setBoxBg

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const mapDispatchToProps = (dispatch) => {
      getColor: (col) => {
       dispatch(getBoxColor(col));
       dispatch(setMessage());
-      dispatch(setBoxBg(col));
+      dispatch(setBoxBg());
      },
 
      setEasy: () => {
