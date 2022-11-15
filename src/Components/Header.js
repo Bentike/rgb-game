313be1f34b24692d50ab8685aa4ace202bd9c685
Bentike <ajadiyusuf73@gmail.com>
2022-11-15 11:14:42 -0800

    added bg prop to header to set its background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Styles/Header.css';
 
-function Header({rgb}) {
+function Header({rgb, bg}) {
   return (
-    <div className='header'>
+    <div className='header' style={{backgroundColor: bg}}>
         <h1>THE GREAT</h1>
         <h1>{rgb}</h1>
         <h2>GUESSING GAME</h2>
